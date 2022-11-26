7e3bc5ac71b4f9630f7ce3145d5701fe831a4932
haechang <94176916+rohhaechang@users.noreply.github.com>
2022-11-26 23:51:00 +0900

    create searchResult

--- pages/search.js
+++ pages/search.js
@@ -1,14 +1,17 @@
 import Head from "next/head";
 import SearchHeader from "../components/SearchHeader";
 import Response from "../Response";
+import SearchResults from "../components/SearchResults";
+import { useRouter } from "next/router";
 
 
 export default function search({results}) {
   console.log(results);
+  const router = useRouter();
   return (
     <div>
       <Head>
-        <title>Search Page cskjbsc</title>  
+        <title>{router.query.term} - Search page</title>  
       </Head>
 
       {/* Search Header */}      
@@ -16,11 +19,15 @@ export default function search({results}) {
       <SearchHeader />
 
       {/** Search Results */}
+
+      <SearchResults results={results} />
     </div>
   )
 }
 
 export async function getServerSideProps(context) {
+  // mockData는 가짜 데이터로, 실제로 서버에서 데이터를 가져오지 않고 미리 만들어놓은 데이터를 가져오는 것
+  // 지금은 서버에서 가져오지 않는 단계이므로, mock data를 만들어놓고, true로 설정하여 Response.js에 json으로 만들어놓은 데이터를 가져오도록 하였다.
   const mockData = true
   const data = mockData ? Response : await fetch(
     `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}${context.query.searchType && "&searchType=image"}`
