a7138e5239b248fe26ce5499f0ba8efa991a03c8
haechang <94176916+rohhaechang@users.noreply.github.com>
2022-11-28 19:43:33 +0900

    fix remaining buttons and im feeling lucky and adding secret env

--- pages/index.js
+++ pages/index.js
@@ -20,6 +20,13 @@ export default function Home() {
     // url을 바꾼다.
     router.push(`/search?term=${term.trim()}&searchType=`);
   }
+  const randomSearch = async (event) => {
+    event.preventDefault();
+    const randomTerm = await fetch('https://random-word-api.herokuapp.com/word?number=1')
+    .then((response) => response.json());
+    if(!randomTerm) return; 
+    router.push(`/search?term=${randomTerm}&searchType=`);
+  }
   return (
     <div>
       <Head>
@@ -47,7 +54,7 @@ export default function Home() {
         </div>
         <div className='flex flex-col sm:flex-row w-[50%] space-y-2 mt-8 sm:space-y-0 sm:space-x-4 justify-center'>
           <button onClick={search} className='btn'>Google Search</button>
-          <button className='btn'>I'm Feeling Lucky</button>
+          <button onClick={randomSearch} className='btn'>I'm Feeling Lucky</button>
         </div>
       </form>
 
